be06d6920349459ee6587e1df43c49a1191da5db
kan <erkan.demiralay@web.de>
2019-10-30 00:01:08 +0100

    - added docker configuration variables

--- falib/db.py
+++ falib/db.py
@@ -1,15 +1,27 @@
+import os
 from databases import Database
 import asyncpg
 
 
 class PostgresConfig:
     def __init__(self):
-        self.user = 'postgres'
-        self.pw = 'postgres'
-        self.host = 'localhost'
-        self.port = '5432'
-        self.driver = 'psycopg2'
-        self.db = 'postgresql'
+        config = os.getenv('FA_API_CONFIG')
+        if config is None:
+            raise ValueError
+        if config == 'ubuntu':
+            self.user = 'postgres'
+            self.pw = 'postgres'
+            self.host = 'localhost'
+            self.port = '5432'
+            self.db = 'postgresql'
+        elif config == 'docker':
+            self.user = 'docker'
+            self.pw = 'docker'
+            self.host = '172.17.0.1'
+            self.port = '5432'
+            self.db = 'postgresql'
+        else:
+            raise NotImplementedError
 
     def get_uri(self, db_name):
         uri = f"{self.db}://{self.user}:{self.pw}@{self.host}:{self.port}/{db_name}"
