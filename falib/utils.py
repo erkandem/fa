f5aaf298d2fd5336288aedc7053699b62d144cf0
kan <erkan.demiralay@web.de>
2019-10-30 15:20:20 +0100

    - added eod_ini_logic and to handle startdate and enddate processing  - added tests but DO rename the variables properly

--- falib/utils.py
+++ falib/utils.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime as dt
+
 from falib.const import prices_etf_sym_choices
 from falib.const import prices_fx_sym_choices
 from falib.const import iv_ice_choices
@@ -31,4 +33,27 @@ async def add_missing_keys(keys: [str], args: dict) -> dict:
     for key in keys:
         if key not in list(args):
                args[key] = None
+    return args
+
+
+def eod_ini_logic(args: {}) -> {}:
+    """
+    assure that the `startdate` and `enddate` keys in the returned dictionary are:
+        - ready for postgres which expects `YYYY-MM-DD`
+        - `startdate` is bigger or equal to `enddate`
+        - are not None
+    """
+    if args['dminus'] is None:
+        args['dminus'] = 20
+    delta_d = timedelta(days=args['dminus'])
+    if args['enddate'] is None:
+        args['enddate'] = dt.now().date().strftime('%Y%m%d')
+    if args['startdate'] is None:
+        args['startdate'] = (dt.strptime(args['enddate'], '%Y%m%d') - delta_d).strftime('%Y%m%d')
+    enddate = dt.strptime(args['enddate'], '%Y%m%d')
+    startdate = dt.strptime(args['startdate'], '%Y%m%d')
+    if startdate > enddate:
+        startdate = enddate
+    args['enddate'] = enddate.strftime('%Y-%m-%d')
+    args['startdate'] = startdate.strftime('%Y-%m-%d')
     return args
---
+++ tests/test_ini_logic.py
@@ -0,0 +1,88 @@
+"""
+
+"""
+from datetime import datetime as dt
+from falib.utils import eod_ini_logic
+
+
+def test_ini_logic():
+    startdate = '20190905'
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': '20191005', 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d').strftime('%Y%m%d')
+    assert check_me == startdate
+
+
+def test_ini_logic_2():
+    startdate = None
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': '20191005', 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d')
+    assert type(check_me.strftime('%Y%m%d')) is str
+
+
+def test_ini_logic_3():
+    startdate = None
+    enddate = None
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': enddate, 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d')
+    check_me2 = dt.strptime(result['enddate'], '%Y-%m-%d')
+    assert type(check_me.strftime('%Y%m%d')) is str
+    assert type(check_me2.strftime('%Y%m%d')) is str
+    assert check_me2  >= check_me
+
+
+def test_ini_logic_4():
+    startdate = None
+    enddate = '20190519'
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': enddate, 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d')
+    check_me2 = dt.strptime(result['enddate'], '%Y-%m-%d')
+    assert type(check_me.strftime('%Y%m%d')) is str
+    assert type(check_me2.strftime('%Y%m%d')) is str
+    assert check_me2  >= check_me
+
+
+def test_ini_logic_5():
+    startdate = '20190519'
+    enddate = '20190519'
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': enddate, 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d')
+    check_me2 = dt.strptime(result['enddate'], '%Y-%m-%d')
+    assert type(check_me.strftime('%Y%m%d')) is str
+    assert type(check_me2.strftime('%Y%m%d')) is str
+    assert check_me2 >= check_me
+
+
+def test_ini_logic_6():
+    startdate = '20190520'
+    enddate = '20190519'
+    args = {
+        'symbol': 'ewz', 'month': 'f', 'year': 2019, 'ust': 'eqt', 'exchange': 'usetf',
+        'startdate': startdate, 'enddate': enddate, 'dminus': 20, 'order': 'asc'
+    }
+    result = eod_ini_logic(args)
+    check_me = dt.strptime(result['startdate'], '%Y-%m-%d')
+    check_me2 = dt.strptime(result['enddate'], '%Y-%m-%d')
+    assert type(check_me.strftime('%Y%m%d')) is str
+    assert type(check_me2.strftime('%Y%m%d')) is str
+    assert check_me2 >= check_me
+
