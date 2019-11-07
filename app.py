0f12cccafe43e5628a425c2e5ea172bf533e78b0
kan <erkan.demiralay@web.de>
2019-11-07 13:09:24 +0100

    - bugfix typo in scheme

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ app.include_router(eod_futures_router)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/token')
 
 origins = [
-    'ttp://localhost',
+    'http://localhost',
     'http://localhost:8080',
 ]
 app.add_middleware(
