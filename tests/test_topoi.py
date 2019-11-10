2f19b13c053015fd439a4e5e31d5cf9bdb415016
kan <erkan.demiralay@web.de>
2019-11-10 02:26:31 +0100

    - sample queries for tests

---
+++ tests/test_topoi.py
@@ -0,0 +1,62 @@
+queries = {
+    'fut_cme_cl':
+        {
+        "ust": "fut",
+        "exchange": "cme",
+        "symbol": "cl",
+        "option_month": "201912",
+        "underlying_month": "201912",
+        "startdate": "20190101",
+        "enddate": "20190401",
+        "putcall": "call",
+        "ltd": "20191115",
+        "metric": "oi",
+        "dminus": 365,
+        "top_n": 5,
+        "order": "desc"
+    }
+    , 'eqt_usetf_spy':
+        {
+        "ust": "eqt",
+        "exchange": "usetf",
+        "symbol": "spy",
+        "startdate": "20190101",
+        "enddate": "20190401",
+        "putcall": "call",
+        "ltd": "20200117",
+        "metric": "oi",
+        "dminus": 365,
+        "top_n": 5,
+        "order": "desc"
+    }
+    ,'fut_ice_b':
+        {
+            "ust": "fut",
+            "exchange": "ice",
+            "symbol": "b",
+            "option_month": "201910",
+            "underlying_month": "201912",
+            "startdate": "20190101",
+            "enddate": "20190401",
+            "putcall": "call",
+            "ltd": "20191028",
+            "metric": "oi",
+            "dminus": 365,
+            "top_n": 5,
+            "order": "desc"
+        }
+    ,'ind_eurex_dax':
+        {
+            "ust": "ind",
+            "exchange": "eurex",
+            "symbol": "dax",
+            "startdate": "20190101",
+            "enddate": "20190401",
+            "putcall": "call",
+            "ltd": "20191220",
+            "metric": "oi",
+            "dminus": 365,
+            "top_n": 5,
+            "order": "desc"
+    }
+}
