5fd62f2da72939b8f5a086fc656924c12f282cdf
kan <erkan.demiralay@web.de>
2019-11-10 02:25:03 +0100

    - added bunch of default informational queries  - and readjusted parameter name put_or_call to putcall

--- src/utils.py
+++ src/utils.py
@@ -83,10 +83,192 @@ async def eod_ini_logic(args: {}) -> {}:
 
 
 async def put_call_trafo(args: {}) -> {}:
-    if args['put_or_call'] == 'put':
-        args['put_or_call'] = 0
-    elif args['put_or_call'] == 'call':
-        args['put_or_call'] = 1
+    if args['putcall'] == 'put':
+        args['putcall'] = 0
+    elif args['putcall'] == 'call':
+        args['putcall'] = 1
     else:
         raise NotImplementedError()
     return args
+
+
+class CinfoQueries:
+    @staticmethod
+    def ust_f(args):
+        return f'''
+            SELECT DISTINCT ust AS ust
+            FROM cinfo 
+            ORDER BY ust;
+        '''
+
+    @staticmethod
+    def ust_where_exchange_f(args):
+        return f'''
+            SELECT DISTINCT ust AS ust
+            FROM cinfo 
+            WHERE exchange = '{args['exchange']}'
+            ORDER BY ust;
+        '''
+
+    @staticmethod
+    def ust_where_symbol_f(args):
+        return f'''
+            SELECT DISTINCT ust AS ust
+            FROM cinfo 
+            WHERE symbol = '{args['symbol']}'
+            ORDER BY ust;
+        '''
+
+    @staticmethod
+    def exchange_f(args):
+        return f'''
+            SELECT DISTINCT exchange AS exchange
+            FROM cinfo 
+            ORDER BY exchange;
+        '''
+
+    @staticmethod
+    def exchange_where_ust_f(args):
+        return f'''
+            SELECT DISTINCT exchange AS exchange
+            FROM cinfo 
+            WHERE ust = '{args['ust']}'
+            ORDER BY exchange;
+        '''
+
+    @staticmethod
+    def exchange_where_symbol_f(args):
+        return f'''
+            SELECT DISTINCT exchange AS exchange
+            FROM cinfo 
+            WHERE symbol = '{args['symbol']}'
+            ORDER BY exchange;
+        '''
+
+    @staticmethod
+    def symbol_f(args):
+        return f'''
+            SELECT DISTINCT symbol AS symbol
+            FROM cinfo 
+            ORDER BY symbol;
+        '''
+
+    @staticmethod
+    def symbol_where_ust_f(args):
+        return f'''
+            SELECT DISTINCT symbol AS symbol
+            FROM cinfo 
+            WHERE ust = '{args['ust']}']
+            ORDER BY ust;
+        '''
+
+    @staticmethod
+    def symbol_where_exchange_f(args):
+        return f'''
+            SELECT DISTINCT symbol AS symbol
+            FROM cinfo 
+            WHERE exchange = '{args['exchange']}'
+            ORDER BY ust;
+        '''
+
+    @staticmethod
+    def symbol_where_ust_and_exchange_f(args):
+        return f'''
+            SELECT DISTINCT symbol AS symbol
+            FROM            cinfo 
+            WHERE           exchange = '{args['exchange']}'
+                AND         ust = '{args['ust']}'
+            ORDER BY        symbol;
+       '''
+
+    @staticmethod
+    def ltd_where_ust_exchange_and_symbol_f(args):
+        return f'''
+        SELECT DISTINCT ltd AS ltd
+        FROM            cinfo 
+        WHERE           symbol = '{args['symbol']}'
+            AND         exchange = '{args['exchange']}'
+            AND         ust = '{args['ust']}'
+        ORDER BY        ltd;
+       '''
+
+    @staticmethod
+    def ltd_date_where_ust_exchange_and_symbol_f(args):
+        return f'''
+        SELECT DISTINCT ltd_date AS ltd
+        FROM            cinfo 
+        WHERE           symbol = '{args['symbol']}'
+            AND         exchange = '{args['exchange']}'
+            AND         ust = '{args['ust']}'
+        ORDER BY        ltd_date;
+       '''
+
+    @staticmethod
+    def get_table_and_schema_by_ltd(args):
+        return f'''
+            SELECT schema_name, table_name 
+            FROM    cinfo
+            WHERE   ust = '{args['ust']}'
+                AND exchange = '{args['exchange']}'
+                AND symbol = '{args['symbol']}'
+                AND ltd = '{args['ltd']}';
+        '''
+
+    @staticmethod
+    def get_schema_and_table_for_futures_sql(args):
+        return f'''
+        SELECT schema_name, table_name
+        FROM cinfo
+        WHERE   symbol = '{args["symbol"]}'
+            AND ust = '{args["ust"]}'
+            AND exchange = '{args["exchange"]}'
+            AND underlying_month = '{args["underlying_month"]}'
+            AND option_month = '{args["option_month"]}'
+            AND ltd = '{args["ltd"]}';
+            '''
+
+    @staticmethod
+    def get_table_and_schema_by_month_and_year(args):
+        raise NotImplementedError(
+            'It is unclear (by design), whether option month and option year'
+            'should actually reference to the month and year of the future. '
+            'My second guess is that it is the second one')
+        return f'''
+            SELECT schema_name, table_name 
+            FROM    cinfo
+            WHERE   ust = '{args['ust']}'
+                AND exchange = '{args['exchange']}'
+                AND symbol = '{args['symbol']}'
+                AND option_month_code = '{args['month']}'
+                AND option_year = '{args['year']}';
+        '''
+
+    @staticmethod
+    def strikes_where_table_f(args):
+        return f'''
+            SELECT DISTINCT strkpx AS strike
+            FROM {args['schema']}.{args['table']}
+            ORDER BY strkpx;
+        '''
+
+    @staticmethod
+    def strikes_where_table_and_pc_f(args):
+        return f'''
+            SELECT DISTINCT strkpx AS strike
+            FROM {args['schema']}.{args['table']}
+            WHERE putcall = {args['putcall']}
+            ORDER BY strkpx;
+        '''
+
+    @staticmethod
+    def first_and_last_date_where_putcall_and_strike_f(args):
+        return f'''
+            SELECT  MIN(BIZDT) as start_date,
+                    MAX(bizdt) as last_date,
+            FROM {args['schema']}.{args['table']}
+            WHERE putcall = {args['putcall']}
+            AND strkpx = {args['strkpx']}
+            GROUP BY strkpx 
+            ORDER BY strkpx;
+        '''
+
