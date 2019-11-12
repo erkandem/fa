ac80de82e32f8d82fad5a35c2c6d92b7e5ffcfe0
kan <erkan.demiralay@web.de>
2019-11-12 22:03:20 +0100

    simplify config

--- src/db.py
+++ src/db.py
@@ -7,39 +7,14 @@ class PostgresConfig:
     def __init__(self):
         import dotenv
         dotenv.load_dotenv('.env')
-        config = os.getenv('FA_API_CONFIG')
-        if config is None:
-            raise ValueError
-        if config == 'ubuntu':
-            self.user = 'postgres'
-            self.pw = 'postgres'
-            self.host = 'localhost'
-            self.port = '5432'
-            self.db = 'postgresql'
-        elif config == 'remote':
-            self.user = os.getenv("PG_USER")
-            self.pw = os.getenv("PG_PW")
-            self.host = 'localhost'
-            self.port = '5432'
-            self.db = 'postgresql'
-        elif config == 'remote_docker':
-            self.user = os.getenv("PG_USER")
-            self.pw = os.getenv("PG_PW")
-            self.host = '172.17.0.1'
-            self.port = '5432'
-            self.db = 'postgresql'
-        elif config == 'docker':
-            self.user = 'docker'
-            self.pw = 'docker'
-            self.host = '172.17.0.1'
-            self.port = '5432'
-            self.db = 'postgresql'
-        else:
-            raise NotImplementedError
+        self.user = os.getenv("PG_USER")
+        self.pw = os.getenv("PG_PW")
+        self.host = os.getenv("PG_HOST")
+        self.port = os.getenv("PG_PORT")
+        self.db = 'postgresql'
 
     def get_uri(self, db_name):
         uri = f"{self.db}://{self.user}:{self.pw}@{self.host}:{self.port}/{db_name}"
-        print(uri)
         return uri
 
 
