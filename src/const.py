62abef316417bef412a011129c4fa65941e89a46
kan <erkan.dem@pm.me>
2019-12-22 19:17:46 +0100

    - prepare for risk reversal endpoint   - added `deltaOffsetChoices` enum and   - respective list of string object `delta_offset_choices`

--- src/const.py
+++ src/const.py
@@ -339,6 +339,17 @@ class deltaChoicesPractical(str, Enum):
     _d090 = 'd090'
 
 
+class deltaOffsetChoices(str, Enum):
+    _d5 = 5
+    _d10 = 10
+    _d15 = 15
+    _d20 = 20
+    _d25 = 25
+    _d30 = 30
+    _d35 = 35
+    _d40 = 40
+
+
 class intradayPricesCmeSymChoices(str, Enum):
     _a6 = 'a6'
     _cl = 'cl'
@@ -491,6 +502,7 @@ offset_steps = get_values(offsetSteps)
 iv_eurex_choices = get_values(iVolEurexChoices)
 iv_ice_choices = get_values(iVolChoicesIce)
 prices_fx_sym_choices = get_values(pricesFxSymChoices)
+delta_offset_choices  = enum_class_string(deltaOffsetChoices)
 
 iv_all_sym_choices = iv_etf_choices + iv_cme_choices + iv_eurex_choices + iv_ice_choices
 
