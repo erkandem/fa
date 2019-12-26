0400c60c81c04fc918e1c7963b9c0c02f45a553c
kan <erkan.dem@pm.me>
2019-12-26 01:23:56 +0100

    - add authorization headers to locust  - adjust locust testing module to changed prices intraday

--- locust_test.py
+++ locust_test.py
@@ -6,24 +6,46 @@ from datetime import datetime as dt, timedelta
 from locust import HttpLocust, TaskSet, task
 from tests.sample_user_agents import ua_strings
 from src.const import prices_etf_sym_choices
+import os
+import json
+import dotenv
 
 
-local_host = 'http://localhost:5000'
+dotenv.load_dotenv(dotenv.find_dotenv())
+CREDS = json.loads(os.getenv('DEFAULT_API_SUPER_USER'))
+local_host = 'https://api.volsurf.com'
+local_host = 'http://0.0.0.0:5000'
+
+
+def get_auth_header(creds, local_client):
+    body = {'username': creds['username'], 'password': creds['password']}
+    login_header = {'Content-Type': 'application/x-www-form-urlencoded'}
+    login_response = local_client.post('/login', data=body, headers=login_header)
+    if login_response.status_code == 200:
+        token = login_response.json()['access_token']
+        header = {'Authorization': f'Bearer {token}'}
+        return header
+    else:
+        print(login_response.status_code)
+        print(login_response.json())
+        raise ValueError
 
 
 class WebsiteTasks(TaskSet):
     headers = {'Accept': 'application/json'}
 
     def on_start(self):
+        auth_header = get_auth_header(CREDS, self.client)
         self.headers['User-Agent'] = random.choices(ua_strings)[0]
-        return
+        self.headers = {**self.headers, **auth_header}
 
     @task(1)
     def index(self):
-        url = '/prices/intraday?symbol={}&iunit={}&interval=1&dminus={}'.format(
+        url = '/prices/intraday?symbol={}&iunit={}&interval={}&dminus={}'.format(
             random.choice(prices_etf_sym_choices),
-            random.choice(['minutes', 'hour', 'day']),
-            random.randint(10, 15)
+            random.choice(['minutes']),
+            random.choice([1, 10]),
+            random.choice([5, 10, 15, 30, 365])
         )
         self.client.get(url)
 
