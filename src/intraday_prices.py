327d1f3140383ab534f980e54afd9a701d1a08cb
kan <erkan.dem@pm.me>
2019-12-26 01:21:37 +0100

    - major refactoring   - add additional case for the simple query   - remove bug prone if/else logic by dict with all     allowed queries   - compose more reasonable error message   - remove `prices_intraday_keys`, `Validator`, `IntradayPricesParams`     and the usage thereof   - add comment to sql

--- src/intraday_prices.py
+++ src/intraday_prices.py
@@ -1,39 +1,53 @@
-from collections import namedtuple
 from datetime import datetime as dt
 from datetime import date
 from datetime import timedelta
 import fastapi
 from pydantic import BaseModel
 from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 from falib.contract import Contract
 from src.const import OrderChoices
-from src.const import ust_choices_intraday
-from src.const import exchange_choices_intraday
-from src.const import futures_month_chars
-from src.const import prices_intraday_all_symbols
 from src.utils import eod_ini_logic
 from src.utils import guess_exchange_and_ust
 from src.db import engines
 from src.users.auth import bouncer
 from src.users.auth import get_current_active_user
 from src.users.user_models import UserPy
-
+from src.const import IntervalUnitChoices, IntervalValueChoices
+from starlette.exceptions import HTTPException
+import json
 
 router = fastapi.APIRouter()
 
-prices_intraday_keys = [
-    'symbol',
-    'month',
-    'year',
-    'ust',
-    'exc',
-    'startdate',
-    'enddate',
-    'dminus',
-    'interval',
-    'iunit',
-    'order'
- ]
+
+allowed_queries_configs = {
+    'month_1': {'unit': 'month', 'value': 1, 'query': 'date_trunc', 'multiplier': None},
+    'week_1': {'unit': 'week', 'value': 1, 'query': 'date_trunc', 'multiplier': None},
+    'day_1': {'unit': 'day', 'value': 1, 'query': 'date_trunc', 'multiplier': None},
+    'hour_2': {'unit': 'hour', 'value': 2, 'query': 'floor_extract', 'multiplier': 60 * 60 * 2},
+    'hour_1': {'unit': 'hour', 'value': 1, 'query': 'date_trunc', 'multiplier': None},
+    'minutes_30': {'unit': 'minutes', 'value': 30, 'query': 'floor_extract', 'multiplier': 60 * 30},
+    'minutes_20': {'unit': 'minutes', 'value': 20, 'query': 'floor_extract', 'multiplier': 60 * 20},
+    'minutes_15': {'unit': 'minutes', 'value': 15, 'query': 'floor_extract', 'multiplier': 60 * 15},
+    'minutes_10': {'unit': 'minutes', 'value': 10, 'query': 'floor_extract', 'multiplier': 60 * 10},
+    'minutes_5': {'unit': 'minutes', 'value': 5, 'query': 'floor_extract', 'multiplier': 60 * 5},
+    'minutes_4': {'unit': 'minutes', 'value': 4, 'query': 'floor_extract', 'multiplier': 60 * 4},
+    'minutes_3': {'unit': 'minutes', 'value': 3, 'query': 'floor_extract', 'multiplier': 60 * 3},
+    'minutes_2': {'unit': 'minutes', 'value': 2, 'query': 'floor_extract', 'multiplier': 60 * 2},
+    'minutes_1': {'unit': 'minutes', 'value': 1, 'query': 'regular', 'multiplier': None}
+ }
+
+allowed_queries = list(allowed_queries_configs)
+unique_units = list(set([elm['unit'] for elm in allowed_queries_configs.values()]))
+valid_configs = {
+    unit: [
+        elm['value']
+        for elm in allowed_queries_configs.values()
+        if elm['unit'] == unit
+        ] for unit in unique_units
+}
+valid_configs_json = json.dumps(valid_configs)
 
 
 class PricesIntradayPy(BaseModel):
@@ -60,36 +74,6 @@ class PricesIntradayQueryPy(BaseModel):
     order: str
 
 
-class Validator:
-    @staticmethod
-    async def interval(x): return x in [1, 2, 5, 10, 15]
-    @staticmethod
-    async def interval_unit(x): return x in ['minutes', 'hour', 'day', 'week']
-    @staticmethod
-    async def order(x): return x.lower() in ['asc', 'desc']
-    @staticmethod
-    async def symbols(x): return x.lower() in prices_intraday_all_symbols
-    @staticmethod
-    async def month(x): return x.lower() in futures_month_chars
-    @staticmethod
-    async def exchange(x): return x.lower() in exchange_choices_intraday
-    @staticmethod
-    async def ust(x): return x.lower() in ust_choices_intraday
-
-
-v = Validator()
-
-
-IntradayPricesParams = namedtuple(
-    'IntradayPricesParams', [
-        'schema', 'table',
-        'iunit', 'multi',
-        'startdate', 'enddate',
-        'order',
-        'limit'
-])
-
-
 @bouncer.roles_required('user')
 @router.get(
     '/prices/intraday',
@@ -104,8 +88,8 @@ async def get_intraday_prices(
         startdate: str = None,
         enddate: str = None,
         dminus: int = 20,
-        interval: int = 1,
-        iunit: str = 'minutes',
+        interval: IntervalValueChoices = IntervalValueChoices._1,
+        iunit: IntervalUnitChoices = IntervalUnitChoices._minutes,
         order: OrderChoices = OrderChoices._asc,
         user: UserPy = fastapi.Depends(get_current_active_user)
 ):
@@ -118,17 +102,20 @@ async def get_intraday_prices(
         'startdate': startdate,
         'enddate': enddate,
         'dminus': dminus,
-        'interval': interval,
-        'iunit': iunit,
-        'order': order.value
+        'interval': interval.value,
+        'iunit': iunit.value,
+        'order': order.value,
+        'limit': 365
     }
-    content = await prices_intraday_content(args)
+    content = await resolve_prices_intraday(args)
     return content
 
 
 async def select_prices_intraday(args: dict):
     """return an executable SQL statement"""
     args = await guess_exchange_and_ust(args)
+    args = await eod_ini_logic(args)
+
     c = Contract()
     c.symbol = args['symbol']
     c.exchange = args['exchange']
@@ -139,87 +126,99 @@ async def select_prices_intraday(args: dict):
         c.contract_month = args['month']
     if c.contract_month and c.contract_yyyy:
         c.contract = f'''{args['symbol']}{args['month']}{args['year']}'''.lower()
-    args = await eod_ini_logic(args)
-    schema = await c.compose_2_part_schema_name()
-    table = await c.compose_prices_intraday_table_name()
-    limit = 260
-    multi = 60
-    iunit = args['iunit']
 
-    sc_flag = False
-    if args['interval'] == 1:
-        sc_flag = True
-    else:
-        if args['iunit'] == 'month':
-            sc_flag = True
-        elif args['iunit'] == 'week':
-            sc_flag = True
-        elif args['iunit'] == 'day':
-            sc_flag = True
-        elif args['iunit'] == 'hour':
-            multi = 60 * 60 * args['interval']
-        elif args['iunit'] == 'minutes':
-            multi = 60 * args['interval']
-
-    sql_params = IntradayPricesParams(
-        schema=schema,
-        table=table,
-        iunit=iunit,
-        multi=multi,
-        startdate=args['startdate'],
-        enddate=args['enddate'],
-        order=args['order'],
-        limit=limit
-    )
-    sql_code = await _cast_to_sql(sql_params, sc_flag=sc_flag)
+    args['schema'] = await c.compose_2_part_schema_name()
+    args['table'] = await c.compose_prices_intraday_table_name()
+    sql_code = await select_price_data(args)
     return sql_code
 
 
-async def _cast_to_sql(args: IntradayPricesParams, *, sc_flag: bool):
-    if sc_flag:
-        sql = await _cast_to_sql_b(args)
+async def _create_hash(unit, interval):
+    return f'{unit}_{interval}'
+
+
+async def select_price_data(args):
+    hash = await _create_hash(args['iunit'], args['interval'])
+    if hash not in allowed_queries:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail=f"received unit: `{args['iunit']}` and interval `{args['interval']}`. Valid pairs are: {valid_configs}")
+    details = allowed_queries_configs[hash]
+    args['multiplier'] = details['multiplier']
+    if details['query'] == 'floor_extract':
+        sql = await select_via_floor_extract(args)
+    elif details['query'] == 'date_trunc':
+        sql = await select_via_date_trunc(args)
+    elif details['query'] == 'regular':
+        sql = await select_regular(args)
     else:
-        sql = await _cast_to_sql_a(args)
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='We fucked up.'
+        )
     return sql
 
 
-async def _cast_to_sql_a(nt: IntradayPricesParams) -> str:
+async def select_via_floor_extract(args) -> str:
     """case, where we truncate low level by converting datetime to epochs"""
     return f'''
-    SELECT (to_timestamp(floor((extract('epoch' FROM dt) / {nt.multi})) * {nt.multi}) AT TIME ZONE 'UTC')  AS dt, 
+    SELECT (to_timestamp(
+                floor((extract('epoch' FROM dt) / {args['multiplier']}))
+                 * {args['multiplier']}
+            ) AT TIME ZONE 'UTC')                          AS dt, 
            MAX(tz_offset)                                  AS tz, 
-           (array_agg(open_value ORDER BY dt)) [1]     AS open, 
+           (array_agg(open_value ORDER BY dt)) [1]         AS open, 
            MAX(high_value)                                 AS high, 
            MIN(low_value)                                  AS low, 
            (array_agg(close_value ORDER BY dt DESC))[1]    AS close,  
            SUM(volume_value)                               AS volume 
-    FROM   {nt.schema}.{nt.table} 
-    WHERE  dt BETWEEN '{nt.startdate}' AND '{nt.enddate}' 
-    GROUP BY floor(extract('epoch' FROM dt) / {nt.multi})  
-    ORDER BY dt  {nt.order}  
-    LIMIT {nt.limit}; 
+    FROM   {args['schema']}.{args['table']} 
+    WHERE  dt BETWEEN '{args['startdate']}' AND '{args['enddate']}' 
+    -- create one integers (although double) for each row
+    -- same like date_trunc() creates unqiue timestamps only with integers**
+    GROUP BY floor(extract('epoch' FROM dt) / {args['multiplier']})  
+    ORDER BY dt  {args['order']}  
+    LIMIT {args['limit']}; 
     '''
 
 
-async def _cast_to_sql_b(nt: IntradayPricesParams) -> str:
-    """case, where the database takes care of truncating data by date"""
+async def select_via_date_trunc(args) -> str:
+    """
+    case, where the database takes care of truncating data by date
+    """
     return f'''
-        SELECT   date_trunc('{nt.iunit}', dt)                    AS dt, 
+        SELECT   date_trunc('{args['iunit']}', dt)                    AS dt, 
                  MAX(tz_offset)                                  AS tz, 
                  (array_agg(open_value ORDER BY dt ASC))[1]      AS open, 
                  MAX(high_value)                                 AS high, 
                  MIN(low_value)                                  AS low, 
                  (array_agg(close_value ORDER BY dt DESC))[1]    AS close, 
                  SUM(volume_value)                               AS volume 
-        FROM     {nt.schema}.{nt.table} 
-        WHERE    dt BETWEEN '{nt.startdate}' AND '{nt.enddate}' 
-        GROUP BY date_trunc('{nt.iunit}', dt) 
-        ORDER BY dt  {nt.order} 
-        LIMIT    {nt.limit}; 
-         '''
+        FROM     {args['schema']}.{args['table']} 
+        WHERE    dt BETWEEN '{args['startdate']}' AND '{args['enddate']}' 
+        GROUP BY date_trunc('{args['iunit']}', dt)
+        ORDER BY dt  {args['order']} 
+        LIMIT    {args['limit']};'''
 
 
-async def prices_intraday_content(args):
+async def select_regular(args) -> str:
+    """
+    case, where unit = minute and value = 1
+    """
+    return f'''
+        SELECT   dt AS dt,
+                 tz_offset AS tz, 
+                 open_value AS open, 
+                 high_value AS high, 
+                 low_value AS low, 
+                 close_value AS close, 
+                 volume_value AS volume 
+        FROM     {args['schema']}.{args['table']} 
+        WHERE    dt BETWEEN '{args['startdate']}' AND '{args['enddate']}' 
+        ORDER BY dt  {args['order']} 
+        LIMIT    {args['limit']};'''
+
+async def resolve_prices_intraday(args):
     sql = await select_prices_intraday(args)
     async with engines['prices_intraday'].acquire() as con:
         data = await con.fetch(query=sql)
