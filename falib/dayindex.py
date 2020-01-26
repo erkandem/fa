647965dd5562f67e6aca48e31cbdf38cc46dac2e
kan <erkan.dem@pm.me>
2020-01-26 18:54:16 +0100

    - cosmetics  - module docstring

--- falib/dayindex.py
+++ falib/dayindex.py
@@ -1,8 +1,11 @@
-
-
-def get_day_index_table_name(schema_name: str) -> str:
-    return f'{schema_name}_bizdt_to_table_name_mapping'
+"""
+Copied methods from py_markets project
+"""
 
 
 def get_day_index_schema_name() -> str:
     return 'public'
+
+
+def get_day_index_table_name(schema_name: str) -> str:
+    return f'{schema_name}_bizdt_to_table_name_mapping'
