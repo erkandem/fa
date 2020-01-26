1f0b2b8c13af1fb26e5c6afb20d04083cd00261f
kan <erkan.dem@pm.me>
2020-01-26 18:56:00 +0100

    - finalized application code to handle query for    - all options for one underlying    - for one day  - next steps:    - data test    - logic test    - resource to get multiple days

--- src/rawdata_all_options.py
+++ src/rawdata_all_options.py
@@ -4,10 +4,16 @@ Route template
 """
 from collections import namedtuple
 from datetime import datetime as dt
-from datetime import date
+from datetime import date as Date
 from datetime import timedelta
+import json
 import fastapi
+from fastapi import Header
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_204_NO_CONTENT
 from falib.contract import ContractSync
+from falib.dayindex import get_day_index_table_name
+from falib.dayindex import get_day_index_schema_name
 from src.const import OrderChoices
 from src.const import tteChoices
 from starlette.status import HTTP_400_BAD_REQUEST
@@ -20,27 +26,32 @@ from src.users.auth import get_current_active_user
 from src.users.user_models import UserPy
 from starlette.responses import Response
 from src.const import time_to_var_func
+from enum import Enum
 
 
 router = fastapi.APIRouter()
 
+from pydantic import BaseModel
+
+
+class ContentType(str, Enum):
+    json: str = 'application/json'
+    csv: str = 'application/csv'
+
 
 @bouncer.roles_required('user')
 @router.get(
-    '/route/me/please',
-    summary='<summary>',
-    operation_id='unique_operation_id>'
+    '/option-data/single-underlying-single-day',
+    summary='Returns all options for one underlying for one (business)day',
+    operation_id='get_all_options_single_underlying_single_day'
 )
-async def unique_operatin_id(
+async def get_all_options_single_underlying_single_day(
         symbol: str,
+        date: Date,
         ust: str = None,
         exchange: str = None,
-        tte: tteChoices = tteChoices._1m,
-        startdate: str = None,
-        enddate: str = None,
-        dminus: int = 30,
-        order: OrderChoices = OrderChoices._asc,
-        user: UserPy = fastapi.Depends(get_current_active_user)
+        accept: ContentType = Header(default=ContentType.json),
+        user: UserPy = fastapi.Depends(get_current_active_user),
 ):
     """
     A route template. Will set appropriate headers and forward
@@ -49,41 +60,92 @@ async def unique_operatin_id(
     - **symbol**: example: 'SPY' or 'spy' (case insensitive)
     - **ust**: underlying security type: 'eqt' e.g.
     - **exchange**: one of: 'usetf', e.g.
-    - **tte**: time until expiry. 1m 3m 12m ...
-    - **startdate**: format: yyyymmdd
-    - **enddate**: format: yyyymmdd
-    - **dminus**: indicate the number of days back from `enddate`
-    - **order**:  sorting order with respect to date
+    - **date**: format: yyyy-mm-dd
     """
     args = {
         'symbol': symbol,
         'ust': ust,
         'exchange': exchange,
-        'tte': tte._value_,
-        'startdate': startdate,
-        'enddate': enddate,
-        'dminus': dminus,
-        'order': order.value
+        'date': date
     }
     content = await resolve_me(args)
-    return Response(content=content, media_type='application/json')
+    if accept.value == 'application/json':
+        return Response(content=content, media_type='application/json')
+    elif accept.value == 'application/csv':
+        return Response(content=to_csv(content), media_type='application/csv')
 
 
-async def select_resolve(args):
-    args = await eod_ini_logic(args)
-    args = await guess_exchange_and_ust(args)
-    sql = f'''
-    WITH data AS (
-        SELECT 1
+async def to_csv(json_str):
+    data = json.loads(json_str)
+    keys = list(data[0])
+    res = [[str(row[key]) for key in keys] for row in data]
+    csv = (
+            ','.join(keys)
+            + '\n'
+            + '\n'.join([','.join([str(row[key]) for key in keys]) for row in data])
     )
-    SELECT json_agg(data) FROM data;
+    return csv
+
+
+async def get_all_relations(args):
+    c = ContractSync(
+        symbol=args['symbol'],
+        security_type=args['ust'],
+        exchange=args['exchange']
+    )
+    schema = get_day_index_schema_name()
+    table = get_day_index_table_name(c.compose_3_part_schema_name())
+    sql = f'''
+        SELECT name 
+        FROM {schema}.{table}
+        WHERE bizdt = '{args["date"]}';
     '''
+    async with engines['options_rawdata'].acquire() as con:
+        relations = await con.fetch(sql)
+    return relations
+
+
+async def select_union_all_ltds(args):
+    args = await guess_exchange_and_ust(args)
+    if isinstance(args['date'], Date):
+        args['date'] = args['date'].strftime('%Y-%m-%d')
+    relations = await get_all_relations(args)
+    if len(relations) == 0:
+        raise HTTPException(
+            status_code=HTTP_204_NO_CONTENT,
+            detail=f'not data for selected date `{args["date"]}`'
+        )
+    c = ContractSync(
+        symbol=args['symbol'],
+        security_type=args['ust'],
+        exchange=args['exchange']
+    )
+    schema = c.compose_3_part_schema_name()
+    sql = 'WITH data AS ('
+    for n, record in enumerate(relations):
+        sql += f'''(
+            SELECT 
+                bizdt, undprice, putcall, 
+                strkpx, settleprice, volume,
+                oi, yte, moneyness,
+                divyield, rfr, rawiv,
+                delta, tv
+            FROM {schema}.{record['name']}
+            WHERE bizdt = '{args["date"]}'
+        '''
+        if n != len(relations) - 1:
+            sql += ') UNION ALL '
+        else:
+            sql += ')'
+    sql += '''        )
+        SELECT json_agg(data) FROM data;
+        '''
     return sql
 
 
 async def resolve_me(args):
-    sql = await select_resolve(args)
-    async with engines['DATABASE_NAME'].acquire() as con:
+    sql = await select_union_all_ltds(args)
+    async with engines['options_rawdata'].acquire() as con:
         data = await con.fetch(sql)
         if len(data) != 0:
             return data[0].get('json_agg')
